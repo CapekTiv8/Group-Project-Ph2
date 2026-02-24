62ca4ee30d479bbc0d0299e0f99bbc5d8e492f88
marcellinohans8-sketch <marcellinohans8@gmail.com>
2026-02-24 18:52:32 +0700

    feat:add-login-register-endpoint

---
+++ middlewares/authentication.js
@@ -0,0 +1,16 @@
+const auth = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) throw { name: "Unauthorized" };
+    const access_token = authorization.split(" ")[1];
+    const payload = verifyToken(access_token);
+    req.LoginInfo = {
+      userId: payload.id,
+      email: payload.email,
+    };
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = auth;
