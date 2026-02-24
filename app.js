602006183f6e6a215bb540352e18371eba47dfb7
Brandon Lawanto <banansbby@gmail.com>
2026-02-25 00:58:49 +0700

    chore: secondary commit to be safe, added proper Schema for database

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express = require("express");
-const {createServer} = require('node:http');
-const {Server} = require('socket.io')
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", //! Adjust this to your frontend URL
-    methods: ["GET", "POST"]
-  }
+    origin: 'http://localhost:5173', //! Adjust this to your frontend URL
+    methods: ['GET', 'POST'],
+  },
 });
 
 const port = 3000;
-const errorHandler = require("./middlewares/errorHandler");
-const AuthController = require("./controller/AuthController");
-const auth = require("./middlewares/authentication");
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const errorHandler = require('./middlewares/errorHandler');
+const AuthController = require('./controller/AuthController');
+const auth = require('./middlewares/authentication');
+const { Message, Room } = require('./models');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/register", AuthController.Register);
-app.post("/login", AuthController.Login);
+app.post('/register', AuthController.Register);
+app.post('/login', AuthController.Login);
 app.use(auth);
 app.use(errorHandler);
 
-let chats = []
+let chats = []; // This will hold all chat messages in memory (for demonstration purposes)
 // Socket.IO connection handler
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('user connected', socket.id);
 
   // chat:message 2. receive message from client
@@ -40,7 +42,7 @@ io.on('connection', (socket) => {
     // chat:message 4. Broadcast all messages to all connected clients
     io.emit('chat:message', chats);
   });
-  
+
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id);
   });
