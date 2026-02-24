70a8865c6231589a6797e46d450e3856771e1dde
Brandon Lawanto <banansbby@gmail.com>
2026-02-25 00:06:14 +0700

    feat: added fully functioning backend side of socket.io integration for real-time chat, intial commit pre-optimisation

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const {createServer} = require('node:http');
+const {Server} = require('socket.io')
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", //! Adjust this to your frontend URL
+    methods: ["GET", "POST"]
+  }
+});
+
 const port = 3000;
 const errorHandler = require("./middlewares/errorHandler");
 const AuthController = require("./controller/AuthController");
@@ -16,6 +27,28 @@ app.post("/auth/login", AuthController.Login);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+let chats = []
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+  console.log('user connected', socket.id);
+
+  // chat:message 2. receive message from client
+  socket.on('chat:message', (msg) => {
+    console.log('message: ' + msg);
+    // chat:message 3. save message to database (here we just push to an array for demonstration)
+    chats.push(msg); //!change this to save in database
+
+    // chat:message 4. Broadcast all messages to all connected clients
+    io.emit('chat:message', chats);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+  });
+
+  socket.emit('chat:message', chats); // chat:message 6. Send existing chats to the newly connected client
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
