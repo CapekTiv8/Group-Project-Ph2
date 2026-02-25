ea52aed599f078a390faffb20446b7a0ef5325c8
Brandon Lawanto <banansbby@gmail.com>
2026-02-25 23:04:24 +0700

    feat: fully deployable and finished, changed url in backend to deployed url from client

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', //! Adjust this to your frontend URL
+    origin: 'https://eyougle.vercel.app/', //! Adjust this to your frontend URL
     methods: ['GET', 'POST'],
   },
 });
